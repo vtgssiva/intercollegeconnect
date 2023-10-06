6cdf1ffd510c90b86997e508ab6b07bc7a2921c5
i500784 <sivakumar.thiyagarajan@sap.com>
2023-10-06 19:07:18 +0530

    backend program initiated

--- Examples/API_Example.py
+++ Examples/API_Example.py
@@ -8,5 +8,10 @@ def welcome():
     return "Hi How are you doing today?"
 
 
+@app.route('/v1/api/login', methods=['GET', 'POST'])
+def login_validation():
+    return "You have successfully logged In"
+
+
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=8087)
